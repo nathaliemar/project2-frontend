c715fa87cce7c5849d5f900550f8254ea4766401
nathaliemar <n.marten.95@gmail.com>
2025-05-22 14:24:11 +0200

    feat: configure Vite for improved build and server settings; add _redirects file for routing

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: "/index.html",
+    },
+  },
+  server: {
+    historyApiFallback: true, // Ensures proper routing in development
+  },
+  base: "./", // Adjust if deploying to a subdirectory
 });
